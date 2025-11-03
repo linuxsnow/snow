0dd7282cb82e07477bb26119518f78fbdd78294d
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-11-03 12:24:40 -0800

    feat: Add nonfree firmware

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,8 +2,15 @@
 
 set -ouex pipefail
 
+# Repository setup
+sed -i 's/main/main contrib non-free non-free-firmware/' /etc/apt/sources.list.d/*
+
 # Initial update
 apt-get update -y
+
+# Add nonfree firmware
+apt-get install -y \
+	firmware-linux-nonfree
 
 # Switch to IWD
 apt-get install -y \
